4f8d90e9cf6f412c9b891c98d494a98de921a2aa
Areeq Hasan <ahasan20@lawrenceville.org>
2020-07-10 16:02:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# When someone goes to lace-track.herokuapp.com/ --> index() gets executed
 @app.route('/')
 def index():
     return render_template("index.html")
+
+# When someone goes to lace-track.herokuapp.com/greeting/Wasi --> greeting gets executed()
+@app.route('/greeting/<name>')
+def greeting(name):
+    return 'Hi, ' + name + '!'
 
 if __name__ == "__main__":
     app.run()
