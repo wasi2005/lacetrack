1abf5f63e4328eb664df6c2e67513823f9466eee
Areeq Hasan <ahasan20@lawrenceville.org>
2020-07-15 17:33:18 -0400

    cleanup

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
- #  _                         _                           _
- # | |   __ _    ___    ___  | |_   _ __    __ _    ___  | | __
- # | |  / _` |  / __|  / _ \ | __| | '__|  / _` |  / __| | |/ /
- # | | | (_| | | (__  |  __/ | |_  | |    | (_| | | (__  |   <
- # |_|  \__,_|  \___|  \___|  \__| |_|     \__,_|  \___| |_|\_\
- #
- # wasi ahmed
-
-
  #  _                                      _
  # (_)  _ __ ___    _ __     ___    _ __  | |_   ___
  # | | | '_ ` _ \  | '_ \   / _ \  | '__| | __| / __|
@@ -85,12 +76,20 @@ def portal():
 
 @app.route("/login")
 def login():
-    # Find out what URL to hit for Google login
+
+    # Request Simulation
+    # -----------------------------------
+    #  +-- lace-track.herokuapp.com/login
+    #  +-- auth.google.com/authorization_endpoint?scope=openid&scope=email&scope=profile&redirect_uri=lace-track.herokuapp.com/login/callback
+    #  +-- lace-track.herokuapp.com/login/callback?code=3838388
+    #  +-- auth.google.com/token_endpoint?redirect_url=lace-track.herokuapp.com/login/callback&code=3838388&authorization_response=lace-track.herokuapp.com/login/callback?code=3838388
+    #  +-- lace-track.herokuapp.com/login/callback?token_openid=kdnhkjfghZdfkjgxkd&token_email=kdnhkjfghZdfkjgxkd&token_profile=fijfjfjiijf
+    #  +-- auth.google.com/userinfo_endpoint?token_openid=kdnhkjfghZdfkjgxkd&token_email=kdnhkjfghZdfkjgxkd&token_profile=fijfjfjiijf
+    #  +-- lace-track.herokuapp.com/login/callback?sub=657&email=wasi.shams.ahmed@gmail.com&picture=wasi.jpeg&users_name=wahmed937
+
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-    # Use library to construct the request for Google login and provide
-    # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
         redirect_uri=request.base_url + "/callback",
@@ -98,25 +97,14 @@ def login():
     )
     return redirect(request_uri)
 
-# lace-track.herokuapp.com/login
-# auth.google.com/authorization_endpoint?scope=openid&scope=email&scope=profile&redirect_uri=lace-track.herokuapp.com/login/callback
-# lace-track.herokuapp.com/login/callback?code=3838388
-# auth.google.com/token_endpoint?redirect_url=lace-track.herokuapp.com/login/callback&code=3838388&authorization_response=lace-track.herokuapp.com/login/callback?code=3838388
-# lace-track.herokuapp.com/login/callback?token_openid=kdnhkjfghZdfkjgxkd&token_email=kdnhkjfghZdfkjgxkd&token_profile=fijfjfjiijf
-# auth.google.com/userinfo_endpoint?token_openid=kdnhkjfghZdfkjgxkd&token_email=kdnhkjfghZdfkjgxkd&token_profile=fijfjfjiijf
-# lace-track.herokuapp.com/login/callback?sub=657&email=wasi.shams.ahmed@gmail.com&picture=wasi.jpeg&users_name=wahmed937
-
 @app.route("/login/callback")
 def callback():
-    # Get authorization code Google sent back to you //
+
     code = request.args.get("code")
 
-    # Find out what URL to hit to get tokens that allow you to ask for
-    # things on behalf of a user
     google_provider_cfg = get_google_provider_cfg()
     token_endpoint = google_provider_cfg["token_endpoint"]
 
-    # Prepare and send a request to get tokens! Yay tokens!
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response = request.url,
@@ -131,19 +119,12 @@ def callback():
         auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
     )
 
-    # Parse the tokens!
     client.parse_request_body_response(json.dumps(token_response.json()))
 
-    # Now that you have tokens (yay) let's find and hit the URL
-    # from Google that gives you the user's profile information,
-    # including their Google profile image and email
     userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body)
 
-    # You want to make sure their email is verified.
-    # The user authenticated with Google, authorized your
-    # app, and now you've verified their email through Google!
     if userinfo_response.json().get("email_verified"):
         unique_id = userinfo_response.json()["sub"]
         users_email = userinfo_response.json()["email"]
@@ -152,8 +133,6 @@ def callback():
     else:
         return "User email not available or not verified by Google.", 400
 
-    # Create a user in your db with the information provided
-    # by Google
     user = User(
         id_ = unique_id,
         name = users_name,
@@ -161,14 +140,11 @@ def callback():
         profile_pic = picture
     )
 
-    # Doesn't exist? Add it to the database.
     if not User.get(unique_id):
         User.create(unique_id, users_name, users_email, picture)
 
-    # Begin user session by logging the user in
     login_user(user)
 
-    # Send user back to homepage
     return redirect(url_for("portal"))
 
 @app.route("/logout")
