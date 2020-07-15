bcf95e481b686d711c585c33c6189fb55b8ad965
wahmed937 <wasgucciiii@gmail.com>
2020-07-15 13:07:01 -0700

    Update database.py

--- database.py
+++ database.py
@@ -2,10 +2,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-from ast import literal_eval
+# from ast import literal_eval
 
 def get_users():
-    firebase_credentials = ast.literal_eval(os.environ['FIREBASE_CREDENTIALS'])
+    firebase_credentials = eval(os.environ['FIREBASE_CREDENTIALS'])
     cred = credentials.Certificate(firebase_credentials)
     firebase_admin.initialize_app(cred)
     db = firestore.client()
