0a08fa6e9beeba94c44a4bac1c298a87cc6d9e7a
wahmed937 <wasgucciiii@gmail.com>
2020-07-20 12:28:00 -0700

    Update models.py

--- models.py
+++ models.py
@@ -31,8 +31,7 @@ class User(UserMixin):
     @staticmethod
     def create(google_user):
         users_ref = get_users()
-        print(google_user.to_dict())
-        users_ref.document(id).set(google_user.to_dict())
+        users_ref.document(google_user.id).set(google_user.to_dict())
         return google_user
 
 class Shoe(object):
